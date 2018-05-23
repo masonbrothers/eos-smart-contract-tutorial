e23feacb34d3c260253fe6b132e52b2195c38262
Mason Brothers <brothers.mason@gmail.com>
2018-05-23 00:27:57 -0600

    Fixed compose and alias files

--- alias.sh
+++ alias.sh
@@ -1,1 +1,6 @@
-alias cleos='docker exec -it eos-smart-contracts_nodeosd_1 cleos'
+container_name=nodeos
+#container_name=eos-smart-contracts_nodeosd_1
+
+alias cleos="docker exec -it $container_name cleos"
+alias nodeos="docker exec -it $container_name nodeos"
+alias keosd="docker exec -it $container_name keosd"
