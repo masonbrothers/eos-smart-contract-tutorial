0c4144da7b21d21105a94128e52cd3f10dd39554
Mason Brothers <brothers.mason@gmail.com>
2018-05-23 01:17:22 -0600

    Updated to eosio/eos-dev

--- alias.sh
+++ alias.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-container_name=nodeos
-#container_name=eos-smart-contracts_nodeosd_1
+#container_name=nodeos
+container_name=eos-smart-contracts_nodeosd_1
 
 alias cleos="docker exec -it $container_name cleos"
 alias nodeos="docker exec -it $container_name nodeos"
 alias keosd="docker exec -it $container_name keosd"
+alias eosiocpp="docker exec -it $container_name eosiocpp"
+
